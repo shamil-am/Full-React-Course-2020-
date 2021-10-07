f508a371cd0f6760a825b0d2db203ba384a42bc7
shamil-am <shamil.mammadov1@gmail.com>
2021-10-07 17:51:21 +0400

    React Hook useEffect has a missing dependency: error fixed on useFetch js

--- react-2021/src/customHook/useFetch.js
+++ react-2021/src/customHook/useFetch.js
@@ -4,18 +4,18 @@ export const useFetch = (url) => {
   const [data, setData] = useState([]);
   const [loaded, setLoaded] = useState(false);
 
-  const getData = async () => {
-    try {
-      const response = await fetch(url);
-      if (!response.ok) return;
-      const result = await response.json();
-      setData(result);
-      setLoaded(true);
-    } catch (error) {
-      console.log(error);
-    }
-  };
   useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) return;
+        const result = await response.json();
+        setData(result);
+        setLoaded(true);
+      } catch (error) {
+        console.log(error);
+      }
+    };
     getData();
   }, [url]);
 
